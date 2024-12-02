2908fc07bc42758d410b308d14b10b2a9ded2eef
Evan Johnson <johnsonev207@gmail.com>
2024-12-01 22:54:15 -0800

    Day 2, part 2 solution

--- Day 2/solution.rb
+++ Day 2/solution.rb
@@ -17,3 +17,26 @@ def num_safe_reports
         count + 1
     end
 end
+
+# Part 2 solution
+def num_safe_reports_with_dampener
+    @reports.reduce(0) do |count, report|
+        next count + 1 if is_safe(report)
+
+        has_safe_dampened_report = (0..report.length - 1).any? do |i|
+            is_safe(report[0...i] + report[i + 1...])
+        end
+        has_safe_dampened_report ? count + 1 : count
+    end
+end
+
+def is_safe(report)
+    sorted_report = report.sort
+    return false if sorted_report != report && sorted_report != report.reverse
+    
+    return false unless report[1...].each_with_index.all? do |level, i|
+        (1..3).cover? (level - report[i]).abs
+    end
+
+    true
+end

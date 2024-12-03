b81385669415c529983a7fe5069c39bafc7b1fd6
Evan Johnson <johnsonev207@gmail.com>
2024-12-02 22:47:41 -0800

    Day 3, part 2 solution

--- Day 3/solution.rb
+++ Day 3/solution.rb
@@ -14,3 +14,22 @@ end
 def part_1
     mul_instructions_sum(parse_input('input.txt'))
 end
+
+def enabled_mul_instructions_sum(memory)
+    enabled = true
+    memory.reduce(0) do |acc, line|
+        valid_instructions = line.scan(/mul\([0-9]+,[0-9]+\)|do\(\)|don't\(\)/)
+        acc + valid_instructions.sum do |i|
+            if i.match?(/mul\([0-9]+,[0-9]+\)/)
+                enabled ? i.scan(/[0-9]+/).map(&:to_i).reduce(:*) : 0
+            else
+                enabled = i.match?(/do\(\)/)
+                0
+            end
+        end
+    end
+end
+
+def part_2
+    enabled_mul_instructions_sum(parse_input('input.txt'))
+end

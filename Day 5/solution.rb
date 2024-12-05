7ba49764a4204e24db75b353c98177f93b10126c
Evan Johnson <johnsonev207@gmail.com>
2024-12-04 22:08:17 -0800

    Day 5, part 2 solution

--- Day 5/solution.rb
+++ Day 5/solution.rb
@@ -5,10 +5,14 @@ def parse_input(file)
     end
 end
 
-def correctly_ordered_updates(rules, updates)
-    updates.filter do |u|
-        rules.all? { |r| r.any? { |i| !u.include?(i) } || u.index(r[0]) < u.index(r[1]) }
+def is_correctly_ordered?(rules, update)
+    rules.all? do |r|
+        r.any? { |i| !update.include?(i) } || update.index(r[0]) < update.index(r[1])
     end
+end
+
+def correctly_ordered_updates(rules, updates)
+    updates.filter { |u| is_correctly_ordered?(rules, u) }
 end
 
 def sum_update_middles(updates)
@@ -18,4 +22,26 @@ end
 def part_1
     rules, updates = parse_input('input.txt')
     sum_update_middles(correctly_ordered_updates(rules, updates))
+end
+
+def order_updates_correctly(rules, updates)
+    updates.map do |u|
+        new_update = u.clone
+        relevant_rules = rules.filter { |r| r.all? { |i| u.include?(i) } }
+        until is_correctly_ordered?(relevant_rules, new_update)
+            relevant_rules.each do |r| 
+                r0_i, r1_i = new_update.index(r[0]), new_update.index(r[1])
+                next if r0_i < r1_i
+                new_update = (new_update - [r[0], r[1]]).insert(r1_i, r[0], r[1])
+            end
+        end
+        new_update
+    end
+end
+
+def part_2
+    rules, updates = parse_input('input.txt')
+    incorrect_updates = updates - correctly_ordered_updates(rules, updates)
+    sum_update_middles(order_updates_correctly(rules, incorrect_updates))
+
 end

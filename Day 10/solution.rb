515072a3d8da1db3d06e8132c815c84e135a591b
Evan Johnson <evan.johnson@gusto.com>
2024-12-10 08:54:28 -0800

    Day 10, part 2 solution

--- Day 10/solution.rb
+++ Day 10/solution.rb
@@ -32,3 +32,31 @@ end
 def part_1
   overall_trailhead_score(parse_input('./input.txt'))
 end
+
+def overall_trailhead_rating(top_map)
+  trailheads = top_map.flat_map.with_index do |l, y|
+    l.filter_map.with_index { |h, x| [y, x] if h == 0 }
+  end
+
+  trailheads.sum do |(y, x)|
+    stack = [[y, x]]
+    acc = 0
+    while !(pos = stack.shift).nil? do
+      b, a = pos
+      next acc += 1 if top_map[b][a] == 9
+
+      next_spots = [[b + 1, a], [b - 1, a], [b, a + 1], [b, a - 1]].filter do |(d, c)|
+        is_on_map = (0...top_map.length).include?(d) && (0...top_map[0].length).include?(c)
+        is_on_map && top_map[d][c] == top_map[b][a] + 1
+      end
+
+      stack.concat(next_spots)
+    end
+    acc
+  end
+end
+
+
+def part_2
+  overall_trailhead_rating(parse_input('./input.txt'))
+end

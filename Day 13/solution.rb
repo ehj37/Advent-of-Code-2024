2bb693068f1ce254493b0ca0f64e468ee5ddb076
Evan Johnson <evan.johnson@gusto.com>
2024-12-13 09:35:30 -0800

    Day 13, part 2 solution

--- Day 13/solution.rb
+++ Day 13/solution.rb
@@ -19,3 +19,30 @@ end
 def part_1
   fewest_tokens_for_all_prizes(parse_input('./input.txt'))
 end
+
+def parse_input_corrected_coordinates(path)
+  File.read(path).split(/\n\n/).map do |c|
+    c.split(/\n/).map.with_index do |l, i|
+      l.scan(/\d+/).map { |s| i < 2 ? s.to_i : 10000000000000 + s.to_i }
+    end
+  end
+end
+
+def winning_combos_for_redux(claw)
+  (ax, ay), (bx, by), (px, py) = claw
+  (i, j) = [(px * by - bx * py) / (ax * by - ay * bx), (ax * py - ay * px) / (ax * by - ay * bx)]
+  ax * i + bx * j == px && ay * i + by * j == py ? [[i, j]] : []
+end
+
+def fewest_tokens_for_all_prizes_redux(claws)
+  claws.sum do |c|
+    wcs = winning_combos_for_redux(c)
+    next 0 unless wcs.any?
+    wc = wcs.min { |(x, y)| 3 * x + y }
+    3 * wc[0] + wc[1]
+  end
+end
+
+def part_2
+  fewest_tokens_for_all_prizes_redux(parse_input_corrected_coordinates('./input.txt'))
+end
